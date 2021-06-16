8e7d0afabeef1e75d81faf1f31c537f9c5728597
eslamheshamm <esl2mhesham@gmail.com>
2021-06-17 00:32:28 +0200

    setup tailwindcss,adding react-hook-form

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				NeueThin: ["NeueThin", "sans-serif"],
+				NeueLight: ["NeueLight", "sans-serif"],
+				NeueBold: ["NeueBold", "sans-serif"],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
