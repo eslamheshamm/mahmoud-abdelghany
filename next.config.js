14f8b6b352c7e3c27ecf241086aee9d0ed67fb4f
eslamheshamm <esl2mhesham@gmail.com>
2021-06-17 12:16:27 +0200

    updating configs

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
 	reactStrictMode: true,
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId }
-	) {
-		return {
-			"/": { page: "/" },
-			"/about": { page: "/about" },
-			"/blog": { page: "/blog" },
-			"/fonts-in-use": { page: "/fonts-in-use" },
-			"/typefaces": { page: "/typefaces" },
-		};
-	},
 };
