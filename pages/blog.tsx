6208a19115c89e1ae86a42b659c0d951befb4685
eslamheshamm <esl2mhesham@gmail.com>
2021-07-21 21:57:44 +0200

    adding word animated

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 import Layouts from "../components/layouts";
-const Blog = () => {
+import { JigsawPuzzle } from "../components/puzzle";
+import Lottie from "lottie-react-web";
+import animation from "../animation.json";
+
+const Blog: React.FC = () => {
+	const [solved, setSolved] = React.useState(false);
 	return (
 		<Layouts>
 			<Head>
@@ -9,8 +15,37 @@ const Blog = () => {
 				<meta name="description" content="Mahmdoud Abd El Ghany" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<section className="flex justify-center items-center">
-				<h1 className="font-NeueBold text-6xl mt-72">Coming Soon</h1>
+			<section className="h-full w-full flex items-center justify-center">
+				{!solved && (
+					<div className="mx-auto">
+						<JigsawPuzzle
+							imageSrc="/letter.svg"
+							rows={2}
+							columns={2}
+							onSolved={() => {
+								setSolved(true);
+							}}
+						/>
+					</div>
+				)}
+				{solved && (
+					<div className="  ">
+						<figure>
+							<Image src="/hello.svg" alt="hello" width={500} height={500} />
+						</figure>
+					</div>
+				)}
+			</section>
+			<section className=" w-6/12 mx-auto">
+				<Lottie
+					options={{
+						animationData: animation,
+						loop: false,
+					}}
+					style={{
+						cursor: `auto`,
+					}}
+				/>
 			</section>
 		</Layouts>
 	);
