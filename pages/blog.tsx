3efa8e64df8a05f02d0bf4cbd1883dfe38455fea
eslamheshamm <esl2mhesham@gmail.com>
2021-08-07 20:52:38 +0200

    etc

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,20 +3,78 @@ import Head from "next/head";
 import Image from "next/image";
 import Layouts from "../components/layouts";
 import { JigsawPuzzle } from "../components/puzzle";
-import Lottie from "lottie-react-web";
+// import Lottie from "lottie-react-web";
 import animation from "../animation.json";
+import { useLottie } from "lottie-react";
+import Lottie from "lottie-react";
 
-const Blog: React.FC = () => {
-	const [solved, setSolved] = React.useState(false);
+const Blog = () => {
+	// const [solved, setSolved] = React.useState(false);
+	const style = {
+		height: 300,
+	};
+	const options = {
+		animationData: animation,
+	};
+	const lottieObj = useLottie(options, style);
+
+	// const Animation = useLottieInteractivity({
+	// 	lottieObj,
+	// 	mode: "scroll",
+	// 	actions: [
+	// 		{
+	// 			visibility: [0, 1],
+	// 			type: "seek",
+	// 			frames: [0, 30],
+	// 		},
+	// 	],
+	// });
+	const interactivity = {
+		mode: "scroll",
+		actions: [
+			{
+				visibility: [0, 1],
+				type: "seek",
+				frames: [0, 30],
+			},
+		],
+	};
+
 	return (
-		<Layouts>
+		<main className="relative w-full h-full overflow-hidden">
+			{/* <RemoveScrollBar /> */}
 			<Head>
 				<title>Blog</title>
 				<meta name="description" content="Mahmdoud Abd El Ghany" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<section className="h-full w-full flex items-center justify-center">
-				{!solved && (
+			<div className="">
+				{" "}
+				<Lottie
+					animationData={animation}
+					style={style}
+					interactivity={interactivity}
+				/>{" "}
+			</div>
+			<section className="  min-h-screen ">
+				{/* <Lottie
+					animationData={animation}
+					style={style}
+					interactivity={Interactivity}
+				/> */}
+
+				{/* <Lottie
+					options={{
+						animationData: animation,
+						loop: false,
+					}}
+					style={{
+						cursor: `auto`,
+					}} */}
+				{/* /> */}
+			</section>
+			<section className="h-full min-h-screen w-full flex items-center justify-center">
+				{/* {!solved && (
 					<div className="mx-auto">
 						<JigsawPuzzle
 							imageSrc="/letter.svg"
@@ -29,25 +87,15 @@ const Blog: React.FC = () => {
 					</div>
 				)}
 				{solved && (
-					<div className="  ">
+					<div>
 						<figure>
 							<Image src="/hello.svg" alt="hello" width={500} height={500} />
 						</figure>
 					</div>
-				)}
+				)} */}
 			</section>
-			<section className=" w-6/12 mx-auto">
-				<Lottie
-					options={{
-						animationData: animation,
-						loop: false,
-					}}
-					style={{
-						cursor: `auto`,
-					}}
-				/>
-			</section>
-		</Layouts>
+		</main>
 	);
 };
+
 export default Blog;
